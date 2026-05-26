a8abc5d3a09b85fae161d94ee265e9367b66b65a
cjcswe <cjcswe@amazon.com>
2026-05-26 19:29:17 +0000

    AWS Transform: SQL Server to PostgreSQL code transformation

--- app/Bookstore.Web/Controllers/ProductsController.cs
+++ app/Bookstore.Web/Controllers/ProductsController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Bookstore.Data;
 using Bookstore.Domain.Products;
-using Microsoft.Data.SqlClient;
+
 
 namespace Bookstore.Web.Controllers
 {
@@ -30,7 +30,7 @@ namespace Bookstore.Web.Controllers
         {
             try
             {
-                string sql = @"EXEC [dbo].[uspGetProductData];";
+                string sql = @"SELECT * FROM uspGetProductData();";
 
                 return await _context.Database.SqlQueryRaw<Product>(sql).ToListAsync();
             }
